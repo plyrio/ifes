283ef2c7e8596b1bde610eb4949f6fd3fda11ae4
Pedro Lyrio <pedrolyrio@ucl.br>
2023-09-22 20:42:59 -0300

    novas atualizações

--- Javascript/Semana 1/entrada_de_dados.js
+++ Javascript/Semana 1/entrada_de_dados.js
@@ -1,4 +1,6 @@
-var salario_bruto = 7500;
+const prompt = require('prompt-sync')();
+var nome = prompt("Digite o seu nome: ")
+var salario_bruto = prompt("Digite o seu salário bruto: ");
 var ir = salario_bruto * (10/100);
 var salario_liquido = salario_bruto - ir;
-console.log("O salario liquido é de R$", salario_liquido+",00");
+console.log("O salario liquido é de R$", salario_liquido+",00"\n);
