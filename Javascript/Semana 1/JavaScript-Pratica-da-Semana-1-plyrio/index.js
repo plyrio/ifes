12caad3f0ba652f66fdcd737b4d5dc7669f2adae
Pedro Lyrio <pedrolyrio@ucl.br>
2023-09-22 20:56:18 -0300

    Atividade pratica da semana 1 de javascript

---
+++ Javascript/Semana 1/JavaScript-Pratica-da-Semana-1-plyrio/index.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+
+// Leia os três números
+var prompt
+// Calcule a soma e a média
+
+// Mostre na tela o valor da soma e média
--- Javascript/Semana 1/entrada_de_dados.js
+++ Javascript/Semana 1/entrada_de_dados.js
@@ -1,6 +1,6 @@
 const prompt = require('prompt-sync')();
 var nome = prompt("Digite o seu nome: ")
-var salario_bruto = prompt("Digite o seu salário bruto: ");
+var salario_bruto = parseFloat(prompt("Digite o seu salário bruto: "));
 var ir = salario_bruto * (10/100);
 var salario_liquido = salario_bruto - ir;
-console.log("O salario liquido é de R$", salario_liquido+",00"\n);
+console.log(nome,"o salario liquido é de R$", salario_liquido+",00");
