35cb1cb538987b4195dd2dfc0bfa8651ba23b66d
Pedro Lyrio <pedrolyrio@ucl.br>
2023-09-22 21:03:14 -0300

    Atividade finalizada de js

--- Javascript/Semana 1/JavaScript-Pratica-da-Semana-1-plyrio/index.js
+++ Javascript/Semana 1/JavaScript-Pratica-da-Semana-1-plyrio/index.js
@@ -1,7 +1,12 @@
 const prompt = require("prompt-sync")();
 
 // Leia os três números
-var prompt
+var num1 = parseInt(prompt("Digite o 1º número: "));
+var num2 = parseInt(prompt("Digite o 2º número: "));
+var num3 = parseInt(prompt("Digite o 3º número: "));
 // Calcule a soma e a média
-
+var soma = (num1 + num2 + num3);
+var media = (soma/3);
 // Mostre na tela o valor da soma e média
+console.log("A soma é: ",soma);
+console.log("A média é: ",media);
