a88e149be10fc7d2223a5e5b18425c9d6cec2f79
Pedro Lyrio <pedrolyrio@ucl.br>
2023-09-25 19:51:34 -0300

    Atividade pratica da semana 2 de javascript

---
+++ Javascript/Semana 2/JavaScript-Pratica-da-Semana-2-plyrio/index.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+idade = parseInt(prompt("Digite sua idade: "));
+tempo = parseInt(prompt("Digite seu tempo de serviço: "));
+salario = parseFloat(prompt("Digite seu salário: "));
+
+// Implementar o comando de decisão para verificar se o cidadão pode, ou não, se aposentar
+
+// Implementar os comandos de decisão aninhados para calcular o salário de aposentadoria (80% ou 60%)
+
+// Implementar os comandos de decisão aninhados para garantir o salário de aposentadoria dentro dos limites inferior (R$ 1212,00) e superior (R$ 7087,22)
