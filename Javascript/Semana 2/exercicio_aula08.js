c04748d119c68cb5ce79832895d107ecbe70b0ca
Pedro Lyrio <pedrolyrio@ucl.br>
2023-09-25 20:24:13 -0300

    mais exercicios resolvidos da semana 2

--- Javascript/Semana 2/exercicio_aula08.js
+++ Javascript/Semana 2/exercicio_aula08.js
@@ -3,10 +3,10 @@ const   prompt = require("prompt-sync")();
 var n1 = parseInt(prompt("Digite o num 1: "));
 var n2 = parseInt(prompt("Digite o num 2: "));
 
-if (n1 > 0 && n2 > 0 ){
-    if (n1 % 2 == 0 && n2 % 2 == 0){
+if ((n1 > 0) && (n2 > 0)){
+    if ((n1 % 2 == 0) && (n2 % 2 == 0)){
         console.log("Os dois são pares;")
-    } else if(n1 % 2 !== 0 && n2 % 2 !== 0){
+    } else if((n1 % 2 !== 0) && (n2 % 2 !== 0)){
         console.log("Os dois são impares;")
     } else {
         console.log("Um par e um ímpar;")
