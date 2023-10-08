d015682bb8cbd1ac4015958a0873a0a8d53841c0
Pedro Lyrio <pedrolyrio@ucl.br>
2023-10-08 20:25:36 -0300

    Atividade da semana de 3 de javascript concluida

--- Javascript/Semana 3/JavaScript-Pratica-da-Semana-3-plyrio/index.js
+++ Javascript/Semana 3/JavaScript-Pratica-da-Semana-3-plyrio/index.js
@@ -1,3 +1,18 @@
 // Implementar comando de repetição (de 1 até 20)
+var sum_par = 0;
+var sum_imp = 0;
 
+for (var i = 1; i <= 20; i++) {
+  var j = i*i;
+  console.log(j);
+  
 // Implementar os comandos de decisão para verificar números quadrados pares ou ímpares
+  
+  if (j % 2 == 0) {
+    var sum_par = sum_par + j;
+  } else {
+    var sum_imp = sum_imp + j;
+  } 
+}
+  console.log("A soma de quadrados pares é: " + sum_par);
+  console.log("A soma de quadrados ímpares é: " + sum_imp);
