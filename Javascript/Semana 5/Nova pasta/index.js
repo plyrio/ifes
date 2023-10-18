8a76ba113c46e0add48e2fc4fdf401dcb8e8470c
Pedro Lyrio <pedrolyrio@ucl.br>
2023-10-17 21:39:47 -0300

    Quase terminando.

--- Javascript/Semana 5/Nova pasta/index.js
+++ Javascript/Semana 5/Nova pasta/index.js
@@ -11,31 +11,35 @@ do {
   var opcao = prompt("Digite sua opção: ");
 
   if (opcao == 1) {
-    console.log("\n\nInserindo Cliente...\n");
+    console.log("\n\nInserindo Jogo...\n");
     let codigo = parseInt(prompt("Digite o código: "));
-    let nome = prompt("Digite o nome: ");
-    let email = prompt("Digite o e-mail: ");
-    let celular = prompt("Digite o celular: ");
-    let estado = prompt("Digite o estado: ");
+    let nome = prompt("Digite o nome do jogo: ");
+    let genero = prompt("Digite o gênero: ");
+    let plataforma = prompt("Digite a plataforma (Mobile, PC...): ");
+    let lancamento = prompt("Digite o ano de lançamento: ");
 
     // Neste trecho estamos declarando um objeto
-    const cliente = {
+    const jogo = {
       codigo: codigo,
       nome: nome,
-      email: email,
-      celular: celular,
-      estado: estado
+      genero: genero,
+      plataforma: plataforma,
+      lancamento: lancamento
     }
     // Chamar a função inserir
+    inserir_jogo(jogo);
   } else if (opcao == 2) {
-    console.log("\n\nExcluindo Cliente...\n");
-    let codigo = prompt("Digite o código do cliente: ");
+    console.log("\n\nExcluindo Jogo...\n");
+    let codigo = parseFloat(prompt("Digite o código do jogo: "));
+    excluir_jogo(codigo);
     // Chamar a função excluir
   } else if (opcao == 3) {
-    console.log("\n\nListando Clientes...\n");
+    console.log("\n\nListando Jogos...\n");
+    listar_jogos();
     // Chamar a função listar
   } else {
     console.log("\n\nSaindo do programa...\n");
+    
   }
 
   prompt("\nEnter para continuar...");
@@ -43,16 +47,22 @@ do {
 } while (opcao != 0)
 
 
-function inserir_cliente(cliente) {
+function inserir_jogo(jogo) {
   // Implementar corpo da função
+  array_games.push(jogo);
+
 }
 
-function excluir_cliente(codigo) {
+function excluir_jogo(codigo) {
   // Implementar corpo da função
+  array_games.splice(codigo);
 }
 
-function listar_clientes() {
+function listar_jogos() {
   // Implementar corpo da função
+  for (var i = 0; i <= array_games.length; i++){
+    console.log(array_games[i]);
+  }
 }
 
 
