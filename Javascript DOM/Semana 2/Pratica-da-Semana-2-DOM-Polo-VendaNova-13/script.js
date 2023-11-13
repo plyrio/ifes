8c35f20a0c0dbf80da514964c9e7c1f47ac81758
Pedro Lyrio <pedrolyrio@ucl.br>
2023-11-13 11:55:28 -0300

    Atividade finalizada e realizada pelo replit

--- Javascript DOM/Semana 2/Pratica-da-Semana-2-DOM-Polo-VendaNova-13/script.js
+++ Javascript DOM/Semana 2/Pratica-da-Semana-2-DOM-Polo-VendaNova-13/script.js
@@ -1,32 +1,60 @@
-var cachorros = [{ "nome": "Thor", "imagem": "dog1" }, { "nome": "Zeca", "imagem": "dog2" }, { "nome": "Zeus", "imagem": "dog3" }, { "nome": "Pingo", "imagem": "dog4" }];
+var cachorros = [
+  { nome: "Thor", imagem: "dog1" },
+  { nome: "Zeca", imagem: "dog2" },
+  { nome: "Zeus", imagem: "dog3" },
+  { nome: "Pingo", imagem: "dog4" },
+];
 
-var gatos = [{ "nome": "Dunga", "imagem": "cat1" }, { "nome": "Mika", "imagem": "cat2" }, { "nome": "Conan", "imagem": "cat3" }, { "nome": "Messi", "imagem": "cat4" }];
+var gatos = [
+  { nome: "Dunga", imagem: "cat1" },
+  { nome: "Mika", imagem: "cat2" },
+  { nome: "Conan", imagem: "cat3" },
+  { nome: "Messi", imagem: "cat4" },
+];
 
-/ Selecionar elemento 'select_animais' ( 1 ponto)
-var animais = document.getElementById('select_animals');
+// Selecionar elemento 'select_animais' ( 1 ponto)
+var select_animais = document.getElementById("select_animais");
 // Selecionar elemento 'container' ( 1 ponto)
-var container = document.getElementById('container');
+var container = document.getElementById("container");
 // Selecionar elemento 'nome' ( 1 ponto)
-var nome = document.getElementById('nome');
+var nome = document.getElementById("nome");
 
 // Adicionar Listener 'input' ao elemento 'select_animais' (1 ponto)
-animais.addEventListener('click', listarAnimais);
+select_animais.addEventListener("input", listarAnimais);
 // Adicionar Listener 'mouseover' ao elemento 'container' (1 ponto)
-container.addEventListener('mouseover', mostrarNome);
+container.addEventListener("mouseover", mostrarNome);
 // Adicionar Listener 'mouseout' ao elemento 'container' (1 ponto)
-container.addEventListener('mousedown', limparNome);
+container.addEventListener("mouseout", limparNome);
 
-function listarAnimais() { // Implementação da função (10 pontos)
+function listarAnimais() {
+  // Implementação da função (10 pontos)
   limparDivContainer();
   // Implementar...
+  if (select_animais.value == "dog") {
+    for (let i = 0; i < cachorros.length; i++) {
+      var dogs = document.createElement("img");
+      dogs.src = "./img/" + cachorros[i].imagem + ".jpg";
+      dogs.alt = cachorros[i].nome;
+      container.appendChild(dogs);
+    }
+  } else {
+    for (let i = 0; i < gatos.length; i++) {
+      var cats = document.createElement("img");
+      cats.src = "./img/" + gatos[i].imagem + ".jpg";
+      cats.alt = gatos[i].nome;
+      container.appendChild(cats);
+    }
+  }
 }
 
-function mostrarNome(e) { // Implementação da função (4 pontos)
+function mostrarNome(e) {
+  // Implementação da função (4 pontos)
   // Implementar...
+  nome.innerText = e.target.alt;
 }
 
 function limparNome() {
-  p.innerText = "";
+  nome.innerText = "";
 }
 
 // Função para limpar as imagens antigas, antes de inserir novas imagens
