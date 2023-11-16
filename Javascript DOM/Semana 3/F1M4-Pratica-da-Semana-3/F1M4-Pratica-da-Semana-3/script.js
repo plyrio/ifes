8f1cc4fe26072e8f03caff5b7ba79cc578d22ba4
Pedro Lyrio <pedrolyrio@ucl.br>
2023-11-16 11:09:41 -0300

    Atividade concluida pelo replit

--- Javascript DOM/Semana 3/F1M4-Pratica-da-Semana-3/F1M4-Pratica-da-Semana-3/script.js
+++ Javascript DOM/Semana 3/F1M4-Pratica-da-Semana-3/F1M4-Pratica-da-Semana-3/script.js
@@ -1,31 +1,33 @@
-(function() {
-  'use strict'
+(function () {
+  "use strict";
 
-  var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll(".needs-validation");
 
-  Array.prototype.slice.call(forms)
-    .forEach(function(form) {
-      form.addEventListener('submit', function(event) {
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
         if (!form.checkValidity()) {
-          form.classList.add('was-validated')
+          form.classList.add("was-validated");
         } else {
-          inserir()
-          form.classList.remove('was-validated')
-          form.reset()
+          inserir();
+          form.classList.remove("was-validated");
+          form.reset();
         }
-        event.preventDefault()
-        event.stopPropagation()
-      }, false)
-    })
-})()
-
+        event.preventDefault();
+        event.stopPropagation();
+      },
+      false
+    );
+  });
+})();
 
 function getLocalStorage() {
-  return JSON.parse(localStorage.getItem('bd_clientes')) ?? [];
+  return JSON.parse(localStorage.getItem("bd_games")) ?? [];
 }
 
-function setLocalStorage(bd_clientes) {
-  localStorage.setItem('bd_clientes', JSON.stringify(bd_clientes));
+function setLocalStorage(bd_games) {
+  localStorage.setItem("bd_games", JSON.stringify(bd_games));
 }
 
 function limparTabela() {
@@ -35,57 +37,65 @@ function limparTabela() {
   }
 }
 
-function atualizarTabela() { // Adaptação da função atualizarTabela (5 pontos)
+function atualizarTabela() {
+  // Adaptação da função atualizarTabela (5 pontos)
   limparTabela();
-  const bd_clientes = getLocalStorage();
+  const bd_games = getLocalStorage();
   let index = 0;
-  for (cliente of bd_clientes) {
-    const novaLinha = document.createElement('tr');
+  for (game of bd_games) {
+    const novaLinha = document.createElement("tr");
     novaLinha.innerHTML = `
         <th scope="row">${index}</th>
-        <td>${cliente.nome}</td>
-        <td>${cliente.email}</td>
-        <td>${cliente.celular}</td>
-        <td>${cliente.estado}</td>
+        <td>${game.nome}</td>
+        <td>${game.desenvolvedora}</td>
+        <td>${game.plataforma}</td>
+        <td>${game.modo}</td>
+        <td>${game.data}</td>
+        <td>${game.genero}</td>
         <td>
             <button type="button" class="btn btn-danger" id="${index}" onclick="excluir(${index})">Excluir</button>
         </td>
-    `
-    document.querySelector('#tabela>tbody').appendChild(novaLinha)
+    `;
+    document.querySelector("#tabela>tbody").appendChild(novaLinha);
     index++;
   }
 }
 
-function inserir() { // Adaptação da função inserir (10 pontos)
-  const cliente = {
-    nome: document.getElementById('nome').value,
-    email: document.getElementById('email').value,
-    celular: document.getElementById('celular').value,
-    estado: document.getElementById('estado').value
-  }
-  const bd_clientes = getLocalStorage();
-  bd_clientes.push(cliente);
-  setLocalStorage(bd_clientes);
+function inserir() {
+  // Adaptação da função inserir (10 pontos)
+  const game = {
+    nome: document.getElementById("nome").value,
+    desenvolvedora: document.getElementById("desenvolvedora").value,
+    plataforma: document.getElementById("plataforma").value,
+    modo: document.getElementById("modo").value,
+    data: document.getElementById("data").value,
+    genero: document.getElementById("genero").value,
+  };
+  const bd_games = getLocalStorage();
+  bd_games.push(game);
+  setLocalStorage(bd_games);
   atualizarTabela();
 }
 
-function excluir(index) { // Adaptação da função excluir (5 pontos)
-  const bd_clientes = getLocalStorage();
-  bd_clientes.splice(index, 1);
-  setLocalStorage(bd_clientes);
+function excluir(index) {
+  // Adaptação da função excluir (5 pontos)
+  const bd_games = getLocalStorage();
+  bd_games.splice(index, 1);
+  setLocalStorage(bd_games);
   atualizarTabela();
 }
 
-function validarCelular() { // Adaptação da função validar (10 pontos)
-  const bd_clientes = getLocalStorage();
-  for (cliente of bd_clientes) {
-    if (celular.value == cliente.celular) {
-      celular.setCustomValidity("Este número de celular já existe!");
-      feedbackCelular.innerText = "Este número de celular já existe!";
+function validarNome() {
+  // Adaptação da função validar (10 pontos)
+  const bd_games = getLocalStorage();
+  for (game of bd_games) {
+    if (nome.value == game.nome) {
+      nome.setCustomValidity("Este jogo já foi cadastrado!");
+      feedbackNome.innerText = "Este jogo já foi cadastrado!";
       return false;
     } else {
-      celular.setCustomValidity("");
-      feedbackCelular.innerText = "Informe o celular corretamente.";
+      nome.setCustomValidity("");
+      feedbackNome.innerText = "Informe o nome do Game.";
     }
   }
   return true;
@@ -93,6 +103,6 @@ function validarCelular() { // Adaptação da função validar (10 pontos)
 
 atualizarTabela();
 // Seleção dos elementos e adição do listener para validação customizada (5 pontos)
-const celular = document.getElementById("celular");
-const feedbackCelular = document.getElementById("feedbackCelular");
-celular.addEventListener('input', validarCelular);
+const nome = document.getElementById("nome");
+const feedbackNome = document.getElementById("feedbackNome");
+nome.addEventListener("input", validarNome);
